c35a6395ef3842b5f53739f14676dbee2004b9ea
TAMILSELVAN S <tamilsanmugam2005@gmail.com>
2025-03-24 11:02:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("button");
+const jokecontent = document.getElementById("jokecontent");
+const joke = document.getElementById("joke");
+
+
+button.onclick = () => {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+    .then(function(response){
+        button.textContent = "NEXT";
+        joke.textContent ="";
+        jokecontent.textContent = response.data.setup;
+        setTimeout(function(){
+            joke.textContent = response.data.punchline;
+        },1500);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+};
+
